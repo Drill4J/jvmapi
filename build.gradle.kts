d6c871ae2d96ba9afc84b7324f1f0412a42afea7
Igor_Kuzminykh@epam.com <Igor_Kuzminykh@epam.com>
2019-08-05 21:51:00 +0300

    reconfigure publication mode. added jvm's headers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
+@file:Suppress("UNUSED_VARIABLE")
+
 import org.apache.tools.ant.taskdefs.condition.Os
-import org.gradle.internal.jvm.Jvm
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeCompilation
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
@@ -8,39 +9,76 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetPreset
 plugins {
     id("org.jetbrains.kotlin.multiplatform") version ("1.3.30")
 }
+
+apply(from = rootProject.file("gradle/publish.gradle"))
+
 repositories {
     mavenCentral()
 }
-val preset =
-        when {
-            Os.isFamily(Os.FAMILY_MAC) -> "macosX64"
-            Os.isFamily(Os.FAMILY_UNIX) -> "linuxX64"
-            Os.isFamily(Os.FAMILY_WINDOWS) -> "mingwX64"
-            else -> throw RuntimeException("Target ${System.getProperty("os.name")} is not supported")
+val presetName =
+    when {
+        Os.isFamily(Os.FAMILY_MAC) -> "macosX64"
+        Os.isFamily(Os.FAMILY_UNIX) -> "linuxX64"
+        Os.isFamily(Os.FAMILY_WINDOWS) -> "mingwX64"
+        else -> throw RuntimeException("Target ${System.getProperty("os.name")} is not supported")
+    }
+
+fun jvmPaths(target: String) =
+    run {
+        val includeBase = file("native")
+            .resolve("nativeInterop")
+            .resolve("cinterop")
+            .resolve(target)
+        val includeAddition = when {
+            target == "linuxX64" -> includeBase.resolve("linux")
+            target == "macosX64" -> includeBase.resolve("darwin")
+            target == "mingwX64" -> includeBase.resolve("win32")
+            else -> throw RuntimeException("We don't know the prefix for ${System.getProperty("os.name")} target")
         }
+        arrayOf(includeBase, includeAddition)
+    }
 
-val jvmPaths =
-        Jvm.current().javaHome.toPath().run {
-            val includeBase = this.resolve("include")
-
-            val includeAddition = when {
-                Os.isFamily(Os.FAMILY_UNIX) -> includeBase.resolve("linux")
-                Os.isFamily(Os.FAMILY_MAC) -> includeBase.resolve("darwin")
-                Os.isFamily(Os.FAMILY_WINDOWS) -> includeBase.resolve("win32")
-                else -> throw RuntimeException("We don't know the prefix for ${System.getProperty("os.name")} target")
-            }
-            arrayOf(includeBase, includeAddition)
-        }
-
+val isDevMode = System.getProperty("idea.active") == "true"
 kotlin {
     targets {
-        createNativeTargetForCurrentOs("jvmapi") {
-            mainCompilation {
-                val jvmapi by cinterops.creating
-                jvmapi.apply {
-                    includeDirs(jvmPaths,"./src/nativeInterop/cinterop")
-                }
+        if (isDevMode)
+            createNativeTargetForCurrentOs("native") {
             }
+        else {
+            mingwX64("windowsX64")
+            linuxX64("linuxX64")
+            macosX64("macosX64")
+        }
+
+    }
+
+    sourceSets {
+        val commonNativeSs = maybeCreate("nativeMain")
+        if (!isDevMode) {
+            val windowsX64Main by getting { dependsOn(commonNativeSs) }
+            val linuxX64Main by getting { dependsOn(commonNativeSs) }
+            val macosX64Main by getting { dependsOn(commonNativeSs) }
+        }
+    }
+    configure(sourceSets) {
+        val srcDir = if (name.endsWith("Main")) "src" else "test"
+        val platform = name.dropLast(4)
+        kotlin.srcDir("$platform/$srcDir")
+
+    }
+    configure(targets) {
+        val targetName = name
+
+        val cm = compilations["main"]
+        if (cm is KotlinNativeCompilation) {
+            File(project.projectDir, "native/nativeInterop/cinterop").listFiles()?.filter { it.isFile() }
+                ?.forEach { file ->
+                    val filePath = file.absolutePath
+                    val name = file.nameWithoutExtension
+                    val objects = cm.cinterops?.create(name)
+                    objects?.defFile(filePath)
+                    objects?.includeDirs(jvmPaths(this.preset?.name!!), "./native/nativeInterop/cinterop")
+                }
         }
     }
 
@@ -50,14 +88,14 @@ kotlin {
 }
 
 fun KotlinMultiplatformExtension.createNativeTargetForCurrentOs(
-        name: String,
-        config: KotlinNativeTarget.() -> Unit = {}
+    name: String,
+    config: KotlinNativeTarget.() -> Unit
 ) {
-    val createTarget = (presets.getByName(preset) as KotlinNativeTargetPreset).createTarget(name)
+    val createTarget = (presets.getByName(presetName) as KotlinNativeTargetPreset).createTarget(name)
     targets.add(createTarget)
     config(createTarget)
+
 }
 
-fun KotlinNativeTarget.mainCompilation(configureAction: Action<KotlinNativeCompilation>) {
+fun KotlinNativeTarget.mainCompilation(configureAction: Action<KotlinNativeCompilation>) =
     compilations.getByName("main", configureAction as Action<in KotlinNativeCompilation>)
-}
