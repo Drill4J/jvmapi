74c94ff444b5deb24f5ef1aa4c2f011ff2704bb4
Igor_Kuzminykh@epam.com <Igor_Kuzminykh@epam.com>
2019-09-04 22:21:12 +0300

    update version to stable 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("com.jfrog.artifactory") version ("4.9.8")
 }
 
-apply(from = "https://gist.githubusercontent.com/IgorKey/e7a0e07428b6e56283d08dbc605bb942/raw/0af997f4044c4d1e1667e9ee67b2ebdf736fabde/publish.gradle")
+apply(from = "https://raw.githubusercontent.com/Drill4J/build-scripts/master/publish.gradle")
 
 repositories {
     mavenCentral()
